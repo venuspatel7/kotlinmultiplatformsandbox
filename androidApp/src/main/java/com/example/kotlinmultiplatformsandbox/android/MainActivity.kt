48c5ad9680d2510f6d13d7a2c4d18eebc04150b9
VENUS PATEL <A00265063@mycambrian.ca>
2023-09-27 00:01:43 -0400

    MainActivity

---
+++ androidApp/src/main/java/com/example/kotlinmultiplatformsandbox/android/MainActivity.kt
@@ -0,0 +1,56 @@
+package com.example.kotlinmultiplatformsandbox.android
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.Divider
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.kotlinmultiplatformsandbox.Greeting
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MyApplicationTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    GreetingView(Greeting().greet())
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun GreetingView(phrases: List<String>) {
+    LazyColumn(
+        contentPadding = PaddingValues(20.dp),
+        verticalArrangement = Arrangement.spacedBy(8.dp),
+    ) {
+        items( phrases) { phrase ->
+            Text(phrase)
+            Divider()
+        }
+    }
+}
+
+@Preview
+@Composable
+fun DefaultPreview() {
+    MyApplicationTheme {
+        GreetingView(listOf("Hello, Android!"))
+    }
+}
